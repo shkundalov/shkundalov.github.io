1033cf4191defa614c96ae608370874a2433b7cd
shkundalov <108649195+shkundalov@users.noreply.github.com>
2024-05-18 02:12:29 +0300

    Update game.js

--- pages/bac/game.js
+++ pages/bac/game.js
@@ -416,14 +416,14 @@ function translateHTML() {
         el.innerText = text;
     })
 }
-function encrypt(text, key){
+function encrypt(text, key="Passphrase"){
     return [...text].map((x, i) => 
     (x.codePointAt() ^ key.charCodeAt(i % key.length) % 255)
      .toString(16)
      .padStart(2,"0")
    ).join('')
 }
-function decrypt(text, key){
+function decrypt(text, key="Passphrase"){
     return String.fromCharCode(...text.match(/.{1,2}/g)
      .map((e,i) => 
        parseInt(e, 16) ^ key.charCodeAt(i % key.length) % 255)
